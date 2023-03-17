ffd78e9ecc52bca86dabaa902390b5fa4808dac9
ROSHAN R <124763836+RosharnK@users.noreply.github.com>
2023-03-17 18:41:19 +0530

    Update contact.js

--- components/contact.js
+++ components/contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
             <input
               type="email"
               id="email"
-              className="shadow-sm text-black font-semibold text-sm rounded-lg w-full p-2.5 dark:text-white "
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light  "
               placeholder="email"
               required
             />
